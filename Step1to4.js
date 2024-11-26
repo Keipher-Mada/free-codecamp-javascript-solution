a27dcf1b9756bbf2a0faa38abd8d8063df741f17
Keipher-Mada <k.dubber17@gmail.com>
2024-11-26 19:39:27 +0000

    first commit

---
+++ Step1to4.js
@@ -0,0 +1,44 @@
+//step1
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+//step2
+function getGrade(score) {
+  if (score === 100) {
+    return 'A++';
+  } else if (score >= 90) {
+    return 'A';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+//step3 & 4
+function hasPassingGrade(score) {
+  return getGrade(score) !== 'F';
+}
+function studentMsg(totalScores, studentScore) {
+  let avg = getAverage(totalScores);
+  let grades = getGrade(studentScore);
+
+  if (grades != 'F') {
+    return 'Class average: ' + avg + '. Your grade: ' + grades + '. You passed the course.';
+  } else {
+    return 'Class average: ' + avg + '. Your grade: ' + grades + '. You failed the course.';
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
